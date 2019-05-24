dc441878a22b39469d1f4fab19d27a658384fe2d
Bogdan Gromov <uberkinder@ya.ru>
2019-05-24 07:16:43 +0300

    Blending (x50 faster)

--- model/_blend.py
+++ model/_blend.py
@@ -10,6 +10,7 @@ from scipy.optimize import minimize
 
 from ..preprocessing import LabelEncoder1D
 
+import time
 
 __all__ = ['BlendRegressor', 'BlendClassifier']
 
@@ -22,9 +23,23 @@ class Blend(LinearModel):
 
     Parameters
     ----------
+    mean : string or callable (default='mean')
+        Select weighted average function:
+
+            - 'mean': Arithmetic mean
+            - 'hmean': Harmonic mean
+            - 'gmean': Geometric mean
+            - 'median': Median
+
+        If callable, expected signature "mean_func(x, weights, **kwargs)".
+
     scoring : string or None (default=None)
         Objective for optimization. If not None, then calculate the optimal
         weights for the blending.
+
+    opt_params : dict, optional (default={'method': 'SLSQP', 'options': {'maxiter': 100000}})
+        Parameters for scipy <minimize> function
+
 
     Attributes
     ----------
@@ -32,14 +47,68 @@ class Blend(LinearModel):
         Estimated weights of blending model.
 
     '''
-    def __init__(self, scoring=None, **opt_params):
+    def __init__(self, mean='mean', scoring=None, opt_params={'method': 'SLSQP',
+        'options': {'maxiter': 100000}}):
 
+        self.mean = mean
         self.scoring = scoring
         self.opt_params = opt_params
 
 
 
     def _fit(self, X, y, weights=None):
+
+        # Save data
+        self.n_features = X.shape[1]
+        self.target = y.name
+
+        # Init weights
+        self.set_weights(weights)
+
+        # Optimization (if scoring is defined)
+        if self.scoring is not None:
+
+            # Define objective & initial guess
+            objective = lambda w: -self.set_weights(w).score(X, y)
+            w0 = self.coef_
+
+            # Define constraints & bounds
+            cons = [{'type': 'eq', 'fun': lambda w: np.sum(w)-1}]
+            bounds = [(0.,1.)]*self.n_features
+
+            # Optimize
+            result = minimize(objective, w0, bounds=bounds, constraints=cons,
+                **self.opt_params)
+
+        return self
+
+
+    def set_weights(self, weights=None):
+        weights = _check_weights(self.n_features, weights)
+        self.coef_ = weights
+        self.intercept_ = .0
+        return self
+
+
+    def score(self, X, y):
+        scorer = get_scorer(self.scoring)
+        return scorer(self, X, y)
+
+
+    def _mean(self, x):
+        mean = get_mean(self.mean)
+        return mean(x, weights=self.coef_)
+
+
+    def _blend(self, X):
+        return self._mean(X).rename(self.target)
+
+
+
+
+class BlendRegressor(Blend, RegressorMixin):
+
+    def fit(self, X, y, weights=None):
         '''
         X : array-like, shape = (n_samples, n_features)
 
@@ -48,63 +117,11 @@ class Blend(LinearModel):
         weights : array-like, shape = [n_features] or None
             Features weights, initial guess in optimization if scoring is
             not None, otherwise use 'weights' as final coefficients. If None,
-            then features are equally weighted. Must be non negative.
+            then features are equally weighted. Must be non-negative.
 
         '''
-
-        # Save data
-        self.n_cols = X.shape[1]
-        self.target = y.name
-
-        # Initial weights
-        weights = np.ones(self.n_cols) if weights is None else weights
-        weights = norm_weights(weights)
-        self._set_weights(weights)
-
-        # Optimization (if scoring is defined)
-        if self.scoring is not None:
-
-            # Define scorer & objective
-            scorer = get_scorer(self.scoring)
-            objective = lambda w: -scorer(self._set_weights(w), X, y)
-
-            # Define constraints & bounds
-            constraints = [{'type': 'eq', 'fun': lambda w: np.sum(w)-1}]
-            bounds = [(0.,1.)]*self.n_cols
-
-            # Define optimizator params
-            opt_params = {'method': 'SLSQP', 'options': {'maxiter': 100000}}
-            opt_params.update(self.opt_params)
-
-            # Optimize
-            result = minimize(objective, self.coef_, constraints=constraints,
-                bounds=bounds, **opt_params)
-
-            self._set_weights(result['x'])
-
-        return self
-
-
-    def _set_weights(self, weights=None):
-        self.coef_ = weights
-        self.intercept_ = .0
-        return self
-
-
-    def _mean(self, x):
-        return np.average(x, weights=self.coef_)
-
-
-    def _blend(self, X):
-        return X.apply(self._mean, axis=1).rename(self.target)
-
-
-
-
-class BlendRegressor(Blend, RegressorMixin):
-
-    def fit(self, X, y, weights=None):
         return self._fit(X, y, weights)
+
 
     def predict(self, X):
         return self._blend(X)
@@ -117,14 +134,27 @@ class BlendClassifier(Blend, ClassifierMixin):
     # WARNING: Binary only
 
     def fit(self, X, y, weights=None):
+        '''
+        X : array-like, shape = (n_samples, n_features)
+
+        y : array-like, shape = (n_samples, )
+
+        weights : array-like, shape = [n_features] or None
+            Features weights, initial guess in optimization if scoring is
+            not None, otherwise use 'weights' as final coefficients. If None,
+            then features are equally weighted. Must be non-negative.
+
+        '''
         self.le = LabelEncoder1D()
         y = self.le.fit_transform(y)
         return self._fit(X, y, weights)
+
 
     def predict_proba(self, X):
         prob = self._blend(X)
         prob = pd.concat([1-prob, prob], axis=1)
         return prob.values
+
 
     def predict(self, X):
         prob = self._blend(X)
@@ -135,45 +165,46 @@ class BlendClassifier(Blend, ClassifierMixin):
 
 
 
-#def _check_weights(x, weights=None):
-#    w = ones_weights(x) if weights is None else weights
-#    w = norm_weights(w)
+def _check_weights(n_features, weights=None):
 
-# Check type
-#if weights is None:
-#    weights = ones_weights(self.X_shape_[1])
-#elif hasattr(weights, '__iter__'):
-#    weights = np.array(weights)
-#else:
-#    raise TypeError('Unknown type for weights: %s' % type(w))
+    # Check type
+    if weights is None:
+        weights = np.ones(n_features)
+    elif hasattr(weights, '__iter__'):
+        weights = np.array(weights)
+    else:
+        raise TypeError('Unknown type for weights: %s' % type(weights))
 
-# Check size
-#msg = 'X and weights have incompatible shapes: \
-#    {} and {}'.format(self.X_shape_, np.shape(weights))
-#assert len(weights) == self.X_shape_[1],
+    # Check size
+    assert len(weights) == n_features, 'X rows and weights must have same length'
 
-# Check non-negativity
-#assert min(weights) >= .0, 'All weights must be non-negative'
+    # Check non-negativity
+    assert min(weights) >= .0, 'All weights must be non-negative'
 
-ones_weights = lambda x: np.ones(len(x))
-norm_weights = lambda weights: np.array(weights)/sum(weights)
+    # Normalize
+    weights = np.array(weights)/sum(weights)
 
-#def _check_weights(x, weights=None):
-#    w = ones_weights(x) if weights is None else weights
-#    w = norm_weights(w)
-#    return w
+    return weights
 
-def weighted_gmean(x, weights=None):
-    w = _check_weights(x, weights)
-    return np.exp(sum(w*np.log(x)))
 
-#def weighted_hmean(x, weights=None):
-#    w = _check_weights(x, weights)
-#    return (sum(w*x**-1))**-1
 
-#MEAN_FUNCS = {
-#    'mean': np.average,
-#    'gmean': weighted_gmean,
-#    'hmean': weighted_hmean,
-#    'median': ws.numpy_weighted_median,
-#}
+
+MEAN_FUNCS = {
+    'mean': lambda x, weights: x.dot(weights),
+    'gmean': lambda x, weights: np.exp(np.log(x).dot(weights)),
+    'hmean': lambda x, weights: 1/(x**-1).dot(weights),
+    'median': ws.numpy_weighted_median,
+}
+
+
+
+
+def get_mean(mean):
+    if mean in MEAN_FUNCS:
+        return MEAN_FUNCS[mean]
+    elif hasattr(mean, '__call__'):
+        return mean
+    else:
+        raise ValueError(
+            'Invalid value for <mean>. Allowed string ' \
+            'values are {}.'.format(mean, set(MEAN_FUNCS)))
