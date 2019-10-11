9f4d84ffd89f25ef47e922b1f0da73f5afad315a
Bogdan Gromov <uberkinder@ya.ru>
2019-10-11 18:19:06 +0300

    Timer

--- utils/_utils.py
+++ utils/_utils.py
@@ -16,17 +16,7 @@ def all_subsets(cols, k_range):
 
 
 
-def secfmt(s):
-    H, r = divmod(s, 3600)
-    M, S = divmod(r, 60)
-    if H:
-        return '{} h {} min {} sec'.format(int(H), int(M), int(S))
-    elif M:
-        return '{} min {} sec'.format(int(M), int(S))
-    elif S >= 1:
-        return '{} sec'.format(int(S))
-    else:
-        return '{} ms'.format(int(S*1000))
+
 
 
 
@@ -137,3 +127,27 @@ def ld2dl(ld):
     '''
     dl = {key: [d[key] for d in ld] for key in ld[0].keys()}
     return dl
+
+
+
+import math
+import time
+
+import numpy as np
+
+class Timer:
+    def __init__(self, text=None):
+        self.text = text
+
+    def __enter__(self):
+        self.cpu = time.clock()
+        self.time = time.time()
+        if self.text:
+            print("{}...".format(self.text))
+        return self
+
+    def __exit__(self, *args):
+        self.cpu = time.clock() - self.cpu
+        self.time = time.time() - self.time
+        if self.text:
+            print("%s: cpu %0.2f, time %0.2f\n" % (self.text, self.cpu, self.time))
