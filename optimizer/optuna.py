692ed658934d85d03f64824838515b4a93f67128
nikneural <nik.mikbsu@mail.ru>
2023-05-01 19:04:50 +0300

    Changes in file optuna.py
    Refactor: Add typing, docstring, comments
    
    - Added typing for functions and classes to make it more clear and prevent errors
    - Added docstring for functions and classes to help with the readability and usability of the code
    - Added comments in the code to explain the functionality and improve readability
    
    Fixes #1

--- optimizer/optuna.py
+++ optimizer/optuna.py
@@ -1,25 +1,61 @@
-import pandas as pd
+from typing import Dict, Any, Union, Optional
+
 import numpy as np
-
 import optuna
+import pandas as pd
 
-from .base import BaseOptimizer, qround
-
-
-__all__ = ['OptunaCV', 'RandomSearchCV']
-
-
+from . import BaseOptimizer, qround
 
 
 class OptunaCV(BaseOptimizer):
+    """
+    An optimizer that uses Optuna to search for hyperparameters.
+    The search space is defined by the `space` attribute of the `BaseOptimizer` superclass.
+    The objective function is defined by the `eval_params` method of the `BaseOptimizer` superclass.
 
+    Parameters:
+    -----------
+    BaseOptimizer : class
+        A superclass that defines the search space and the objective function.
 
-    def _get_space(self, base_space):
+    Attributes:
+    -----------
+    study : optuna.Study
+        An Optuna study object that contains the hyperparameters found by the search.
+    """
+
+    @staticmethod
+    def _get_space(base_space: Dict[str, Any]) -> Dict[str, Union[tuple, list]]:
+        """
+        Returns the search space as defined by the superclass.
+
+        Parameters
+        ----------
+        base_space : dict
+            The search space as defined by the superclass.
+
+        Returns
+        -------
+        space : dict
+            The search space as defined by the superclass.
+        """
         return base_space
 
+    def _get_params(self,
+                    trial: optuna.Trial) -> Dict[str, Any]:
+        """
+        Returns a dictionary with hyperparameters sampled from the search space.
 
-    def _get_params(self, trial):
+        Parameters
+        ----------
+        trial : optuna.Trial
+            An Optuna trial object that is used to sample hyperparameters from the search space.
 
+        Returns
+        -------
+        params : dict
+            A dictionary with hyperparameters sampled from the search space.
+        """
         space = self.space
         params = {}
 
@@ -50,9 +86,28 @@ class OptunaCV(BaseOptimizer):
 
         return params
 
+    def _fit(self,
+             X: pd.DataFrame,
+             y: pd.Series,
+             groups: np.array = None) -> 'OptunaCV':
+        """
+        Runs the hyperparameter search using Optuna.
+        Returns self.
 
-    def _fit(self, X, y, groups=None):
+        Parameters
+        ----------
+        X : pd.DataFrame
+            The input data.
+        y : pd.Series
+            The target values.
+        groups : np.ndarray or None, optional (default=None)
+            The groups for cross-validation.
 
+        Returns
+        -------
+        self :
+            Opt
+        """
         optuna.logging.set_verbosity(optuna.logging.FATAL)
         optuna.logging.disable_default_handler()
 
@@ -71,25 +126,69 @@ class OptunaCV(BaseOptimizer):
         return self
 
 
-
-
 class RandomSearchCV(OptunaCV):
+    """
+    Random search cross-validation using Optuna library for hyperparameter optimization.
 
-    def _fit(self, X, y, groups=None):
+    Parameters:
+    -----------
+    base_estimator: object
+        A machine learning estimator that will be optimized using Optuna.
+    param_distributions: dict
+        Dictionary with parameters names (str) as keys and distributions or lists of parameters to try.
+    n_trials: int, default=10
+        The number of trials (hyperparameter settings) to try.
+    scoring: str or callable, default=None
+        A string (see sklearn.metrics.SCORERS) or a scorer callable object.
+    n_jobs: int, default=-1
+        Number of jobs to run in parallel. Set to -1 to use all available CPU cores.
+    cv: int or callable, default=5
+        If an integer, specifies the number of folds in a KFold cross-validation strategy.
+        If a callable, this should be a cross-validation strategy that follows scikit-learn API.
+    random_state: int or None, default=None
+        Random seed used to initialize the random number generator.
+    verbose: int, default=0
+        Controls the verbosity: the higher, the more messages.
+    """
+    def _fit(self,
+             X: pd.DataFrame,
+             y: pd.Series,
+             groups: Optional[pd.Series] = None) -> 'RandomSearchCV':
+        """
+        Fit the RandomSearchCV object to the input data using Optuna library.
 
+        Parameters
+        ----------
+        X : pd.DataFrame
+            The input data.
+
+        y : pd.Series
+            The output data.
+
+        groups : pd.Series, optional
+            The groups to use when splitting the data into folds.
+
+        Returns
+        -------
+        self : RandomSearchCV
+            The fitted RandomSearchCV object.
+        """
         optuna.logging.set_verbosity(optuna.logging.FATAL)
         optuna.logging.disable_default_handler()
 
-        def objective(trial):
+        # Mute the logging from the Optuna library
+        def objective(trial: optuna.Trial) -> float:
             params = self._get_params(trial)
             score = self.eval_params(params, X, y, groups)
             return score
 
         if not hasattr(self, 'study'):
             # TODO: set seed & other params
+            # Set the random sampler as the default if no sampler is specified
             sampler = optuna.samplers.RandomSampler(seed=0)
             self.study = optuna.create_study(direction='maximize', sampler=sampler)
 
+        # Run the optimization process
         self.study.optimize(objective)
 
         return self
