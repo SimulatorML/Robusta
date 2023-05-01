8982f57f1aede900becd8cdac2640dd6940cc26a
nikneural <nik.mikbsu@mail.ru>
2023-05-01 23:13:48 +0300

    Changes in file _timer.py
    Refactor: Add typing, docstring, comments
    
    - Added typing for functions and classes to make it more clear and prevent errors
    - Added docstring for functions and classes to help with the readability and usability of the code
    - Added comments in the code to explain the functionality and improve readability
    
    Fixes #1

--- utils/_timer.py
+++ utils/_timer.py
@@ -1,47 +1,112 @@
-import time
 import datetime
-
-import numpy as np
-
-
-
-# Source: https://github.com/yaxen/santander-product-recommendation-8th-place/blob/master/utils.py
-class Timer:
-
-    def __init__(self, text=None):
-        self.text = text
-
-    def __enter__(self):
-        self.cpu = time.clock()
-        self.time = time.time()
-        if self.text:
-            logmsg("{}...".format(self.text))
-        return self
-
-    def __exit__(self, *args):
-        self.cpu = time.clock() - self.cpu
-        self.time = time.time() - self.time
-        if self.text:
-            logmsg("{}: cpu {}, time {}\n".format(self.text, secfmt(self.cpu), secfmt(self.time)))
+import time
+from typing import Optional
 
 
+def logmsg(msg: str) -> None:
+    """
+    Prints a message with a timestamp in the format of [HH:MM:SS].
 
-def secfmt(s):
+    Parameters
+    ----------
+    msg : str
+        A string message to be logged. Can contain newlines.
+
+    Returns
+    -------
+    Nothing:
+        None
+    """
+
+    # split the message into separate lines
+    for m in msg.split('\n'):
+        # get the current time and format it as [HH:MM:SS]
+        t = datetime.datetime.now().strftime("[%H:%M:%S]")
+        # print the timestamp and the message line
+        print(t, m)
+        # pause briefly to avoid overloading the output stream
+        time.sleep(0.01)
+
+def secfmt(s: float) -> str:
+    """
+    Formats a number of seconds as a string.
+
+    Parameters
+    ----------
+    s : float
+        The number of seconds to be formatted.
+
+    Returns
+    -------
+    str:
+        A string representing the formatted time, in the format of "H h M min S sec" or "M min S sec" or
+        "S sec" or "X ms".
+    """
+    # convert seconds to hours, minutes, and seconds
     H, r = divmod(s, 3600)
     M, S = divmod(r, 60)
+
+    # check if there are hours, minutes, or just seconds
     if H:
+        # format as "H h M min S sec"
         return '{} h {} min {} sec'.format(int(H), int(M), int(S))
     elif M:
+        # format as "M min S sec"
         return '{} min {} sec'.format(int(M), int(S))
     elif S >= 1:
+        # format as "S sec"
         return '{} sec'.format(int(S))
     else:
-        return '{} ms'.format(int(S*1000))
+        # format as "X ms"
+        return '{} ms'.format(int(S * 1000))
 
+class Timer:
+    """
+    A context manager that measures CPU time and wall-clock time.
 
+    Usage:
+    with Timer('some operation'):
+        # code to be timed
+    """
 
-def logmsg(msg):
-    for m in msg.split('\n'):
-        t = datetime.datetime.now().strftime("[%H:%M:%S]")
-        print(t, m)
-        time.sleep(0.01)
+    def __init__(self,
+                 text: Optional[str] = None):
+        self.text = text
+
+    def __enter__(self) -> 'Timer':
+        """
+        Called when the Timer is entered.
+
+        Returns
+        -------
+        Timer:
+            The Timer object.
+        """
+        # record the starting CPU time and wall-clock time
+        self.cpu = time.clock()
+        self.time = time.time()
+
+        # log the start of the timed operation, if a description is provided
+        if self.text:
+            logmsg("{}...".format(self.text))
+
+        # return the Timer object
+        return self
+
+    def __exit__(self,
+                 *args: tuple) -> None:
+        """
+        Called when the Timer is exited.
+
+        Parameters
+        ----------
+        *args : tuple
+            Exception information, if any.
+        """
+        # calculate the elapsed CPU time and wall-clock time
+        self.cpu = time.clock() - self.cpu
+        self.time = time.time() - self.time
+
+        # log the results of the timed operation, if a description is provided
+        if self.text:
+            logmsg("{}: cpu {}, time {}\n".format(self.text, secfmt(self.cpu), secfmt(self.time)))
