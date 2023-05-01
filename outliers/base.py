33e89e2b7eb06b7cb05cbab7265c8ce417be0b8c
nikneural <nik.mikbsu@mail.ru>
2023-05-01 19:07:32 +0300

    Changes in file base.py
    Refactor: Add typing, docstring, comments
    
    - Added typing for functions and classes to make it more clear and prevent errors
    - Added docstring for functions and classes to help with the readability and usability of the code
    - Added comments in the code to explain the functionality and improve readability
    
    Fixes #1

--- outliers/base.py
+++ outliers/base.py
@@ -1,21 +1,52 @@
-import pandas as pd
-import numpy as np
+from typing import Optional, Tuple
 
+import pandas as pd
 from sklearn.base import BaseEstimator, OutlierMixin
 
 
-__all__ = ['OutlierDetector']
-
-
-
-
 class OutlierDetector(BaseEstimator, OutlierMixin):
+    """
+    OutlierDetector is a custom class that inherits from BaseEstimator and OutlierMixin.
+    It can be used to detect and remove outliers from a given dataset.
 
+    Parameters:
+    -----------
+    None
+
+    Returns:
+    --------
+    Resampled version of X and y with the outliers removed.
+    """
     _estimator_type = 'outlier_detector'
 
-    def fit_resample(self, X, y=None):
+    def fit_resample(self,
+                     X: pd.DataFrame,
+                     y: Optional[pd.Series] = None) -> Tuple[pd.DataFrame, Optional[pd.Series]]:
+        """
+        Fits the model to the input data X and returns a resampled version of X and y with the outliers removed.
 
+        Parameters
+        ----------
+        X : pd.DataFrame
+            The input data of shape (n_samples, n_features).
+
+        y : Optional[pd.Series]
+            The target labels of shape (n_samples,) (default=None).
+
+        Returns
+        -------
+        X_resampled : np.ndarray
+            The resampled version of X with the outliers removed.
+
+        y_resampled : Optional[np.ndarray]
+            The resampled version of y with the outliers removed (only if y is not None).
+        """
+
+        # Use the fit_predict method of the class to get predicted labels for input data X
         labels = self.fit_predict(X, y)
+
+        # Set the boolean mask for outliers as True where predicted labels are less than 0 (outliers)
         out = labels < 0
 
+        # Return the resampled version of input data X and target labels y with the outliers removed
         return X[~out], y[~out]
