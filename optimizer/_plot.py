737f33159c62378290c4c868ff0d4fb2978cd6ee
RANJITH ROSAN <91519971+RANJITHROSAN17@users.noreply.github.com>
2023-05-01 21:57:58 +0530

    Update _plot.py

--- optimizer/_plot.py
+++ optimizer/_plot.py
@@ -1,22 +1,14 @@
 from matplotlib.ticker import MaxNLocator
-import matplotlib.pylab as plt
+import matplotlib.pyplot as plt
 import seaborn as sns
-
 import numpy as np
 
-
-
 def _plot_progress(opt, marker='.', color='#eeaa24', alpha=0.8):
-
     fig, ax = plt.subplots(1,1)
-
     ax.set_title(type(opt).__name__)
     ax.set_xlabel('iters')
     ax.set_ylabel('score')
-
-    trials = opt.trials_
-
-    sns.regplot(trials.index+1, 'score', trials, color=color)
-
+    trials = opt.trials
+    sns.regplot(trials.index+1, 'score', data=trials, color=color)
     ax.xaxis.set_major_locator(MaxNLocator(integer=True))
-    fig.show()
+    plt.show()
