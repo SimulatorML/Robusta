5a3e1d55fa58e4ef4a2d41dd1ea544df4c8c2992
nikneural <nik.mikbsu@mail.ru>
2023-05-01 17:03:08 +0300

    Changes in file plot.py
    Refactor: Add typing, docstring, comments
    
    - Added typing for function to make it more clear and prevent errors
    - Added docstring for function to help with the readability and usability of the code
    - Added comments in the code to explain the functionality and improve readability
    
    Fixes #1

--- importance/plot.py
+++ importance/plot.py
@@ -1,3 +1,5 @@
+from typing import List, Optional
+
 import pandas as pd
 import numpy as np
 
@@ -5,14 +7,36 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 
 
-def plot_importance(imps, features=None, k_top=30):
+def plot_importance(imps: List[pd.Series],
+                    features: Optional[List[str]] = None,
+                    k_top: Optional[int] = 30) -> None:
+    """
+    Plot feature importance using barplot.
 
+    Parameters
+    ----------
+    imps : List[pd.Series]
+        List of pandas Series, each containing feature importances.
+    features : Optional[List[str]]
+        list of str, names of features to be plotted. If None, the top k_top features will be plotted.
+    k_top : Optional[int]
+        int, number of top features to plot.
+
+    Returns
+    -------
+    Nothing :
+        None.
+    """
+    # Calculate mean importance across all input importances
     imps = [pd.Series(imp) for imp in imps]
     imp = pd.concat(imps, axis=1)
     imp_mean = imp.mean(axis=1)
+
+    # Sort features by mean importance and select top k_top features
     imp_sort = imp_mean.sort_values(ascending=False)
     features = list(imp_sort[:k_top].index) if not features else features
 
+    # Merge all importances into a single pandas Series
     imp_vals = [x.values for x in imps]
     imp_inds = [x.index for x in imps]
     imp_vals = np.hstack(imp_vals)
@@ -21,6 +45,7 @@ def plot_importance(imps, features=None, k_top=30):
     imp.index.name = 'feature'
     imp.name = 'importance'
 
+    # Plot feature importance barplot
     sns.barplot(x='importance', y='feature', data=imp.loc[features].reset_index(),
                 edgecolor=('#d4c3a3'), linewidth=2, palette="inferno_r")
     plt.grid(False)
